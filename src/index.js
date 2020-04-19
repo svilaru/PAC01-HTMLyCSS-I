baa276b40033dcdb7cfcbe3381470e5b3ef86251
Sebastian Vila Ruano <svilaru@uoc.edu>
2020-04-19 21:13:20 +0200

    First version

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import responsivemenu from './assets/js/responsive-menu';
+
+const name = 'Modern WorkFlow';
+console.log(`Hello ${name}`);
+
+let brand = document.querySelector('.c-main-menu__brand');
+
+brand.addEventListener('mouseenter', function(event) {
+  event.target.style.transform = 'rotateY(180deg)';
+});
+
+brand.addEventListener('mouseleave', function(event) {
+  event.target.style.transform = 'rotateY(0deg)';
+});
