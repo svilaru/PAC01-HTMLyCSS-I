3842480ca9d2d94bd880ebbd165045ba45957e48
Sebastian Vila Ruano <svilaru@uoc.edu>
2020-05-17 23:44:18 +0200

    Segunda PAC

--- src/assets/js/responsive-menu.js
+++ src/assets/js/responsive-menu.js
@@ -1,4 +1,4 @@
-const openMobMenu = document.querySelector('.c-main-menu__hamburguer');
+/*const openMobMenu = document.querySelector('.c-main-menu__hamburguer');
 const closeMobMenu = document.querySelector('.c-main-menu__close');
 const menu = document.querySelector('.c-main-menu__list');
 
@@ -12,6 +12,15 @@ closeMobMenu.addEventListener('click', () => {
   menu.classList.remove('c-main-menu__list--isactive');
   openMobMenu.classList.remove('c-main-menu__hamburguer--noactive');
   closeMobMenu.classList.remove('c-main-menu__close--isactive');
+});*/
+
+const openMobMenu = document.querySelector('.c-main-menu__hamburguer');
+const menu = document.querySelector('.c-main-menu__list');
+
+openMobMenu.addEventListener('click', () => {
+  menu.classList.toggle('c-main-menu__list--isactive');
+  openMobMenu.classList.toggle('c-main-menu__hamburguer--noactive');
 });
 
 /*Atención, classlist tiene la función toogle() que permite simplicar el código js*/
+/* Actualizado ;) */
